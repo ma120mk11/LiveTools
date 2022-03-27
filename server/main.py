21f033d8df4ff1004f16d12076666257d2d1c929
Ove <ove.niemisto@gmail.com>
2022-03-27 16:24:41 +0300

    Python fastapi config

--- server/main.py
+++ server/main.py
@@ -316,4 +316,4 @@ async def websocket_endpoint(websocket: WebSocket, client_id: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="192.168.34.249", port=8000)
