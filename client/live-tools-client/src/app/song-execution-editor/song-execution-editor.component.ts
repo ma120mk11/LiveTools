1391d55dcc98553f8babef522af6decef3a5cab1
Ove <ove.niemisto@gmail.com>
2022-07-09 18:49:03 +0300

    Cleanup

--- client/live-tools-client/src/app/song-execution-editor/song-execution-editor.component.ts
+++ client/live-tools-client/src/app/song-execution-editor/song-execution-editor.component.ts
@@ -1,17 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
-import { AngularEditorConfig } from '@kolkov/angular-editor';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, NgForm } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { ISong } from '../components/song-book/song';
-import { SongsService } from '../components/song-book/songs.service';
 import { ILightCommand } from '../light-commands/light-commands.component';
 
-interface ICuelist {
-  name: string,
-  osc: string
-}
 
 @Component({
   selector: 'app-song-execution-editor',
@@ -27,23 +21,23 @@ export class SongExecutionEditorComponent implements OnInit {
   cuelists: ILightCommand[] = []
   cuelistArray: ILightCommand[] = []
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {song: ISong, view: string}, private http: HttpClient, formBuilder: FormBuilder) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {song: ISong, view: string}, private http: HttpClient, private dialogRef: MatDialogRef<SongExecutionEditorComponent>) {
     this.song = {...data.song}
     this.fetchCuelists()
   }
+
   ngOnInit(): void {}
 
   fetchCuelists() {
     this.http.get<ILightCommand[]>(`${environment.apiEndpoint}/lights/commands`)
     .subscribe((result) => {
       this.cuelistArray = result;
-      console.log(this.cuelistArray)
     })
   }
 
   onSubmit() {
     console.log(this.song.execution)
     this.http.post(`${environment.apiEndpoint}/songs/${this.song.id}/execution`, this.song.execution)
-    .subscribe()
+    .subscribe((result) => this.dialogRef.close())
   }
 }
